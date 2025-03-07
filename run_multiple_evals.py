8a0f444241f7893b2509dcccf51285be7e64760b
PierreChouteau <pierrechouteau337@gmail.com>
2025-03-07 10:47:08 +0100

    Clean the repo

--- run_multiple_evals.py
+++ run_multiple_evals.py
@@ -23,37 +23,26 @@ models_to_evaluate = [
 
 eval_mode='default' # default evaluation
 # eval_mode='fast' # fast evaluation
-# eval_mode='robustness' # run many unique evaluations for each model, following different types of robustness tests
-# eval_mode='robustness_vad' # run many unique evaluations for each model, with vad error tests only
 
 
 
 for tag in models_to_evaluate:
     
-    if eval_mode=='robustness_vad' :
-        for i in range(5):
-            command="python eval_robustness_tests.py --tag '{}' --f0-from-mix --test-set 'CSD' --teststocompute baseline_gtf0 gtf0_strict_error_percent --vadseed {}".format(tag,i)
-            print(command)
-            os.system(command)
-    else:
-        if eval_mode=='original_paper':
-            command="python eval.py --tag '{}' --f0-from-mix --test-set 'CSD'".format(tag)
-    
-        elif eval_mode=='default':
-            # mf0 extract with Crepe
-            command="python eval.py --tag '{}' --test-set 'CSD' --show-progress --compute all".format(tag)
-            # mf0 extract with Cuesta et al. (model 3) model
-            # command="python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute all".format(tag)
+    if eval_mode=='original_paper':
+        command=f"python eval.py --tag '{tag}' --f0-from-mix --test-set 'CSD'"
+
+    elif eval_mode=='default':
+        # mf0 extract with Crepe
+        command=f"python eval.py --tag '{tag}' --test-set 'CSD' --show-progress --compute all"
         
-        elif eval_mode=='fast':
-            command="python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute SI-SDR_mask".format(tag)
+        # mf0 extract with Cuesta et al. (model 3) model
+        # command=f"python eval.py --tag '{tag}' --f0-from-mix --test-set 'CSD' --show-progress --compute all"
+    
+    elif eval_mode=='fast':
+        command=f"python eval.py --tag '{tag}' --f0-from-mix --test-set 'CSD' --show-progress --compute SI-SDR_mask"
 
-        elif eval_mode=='robustness':
-            command="python eval_robustness_tests.py --tag '{}' --f0-from-mix --test-set 'CSD' --teststocompute all".format(tag)
+    else:
+        raise ValueError("eval_mode not recognized. Please choose 'default' or 'fast'")
 
-        elif eval_mode=='robustness_vad':
-            command="python eval_robustness_tests.py --tag '{}' --f0-from-mix --test-set 'CSD' --teststocompute baseline_gtf0 gtf0_strict_error_percent --vadseed 0".format(tag)
-
-
-        print(command)
-        os.system(command)
+    print(command)
+    os.system(command)
